b27110a397dda674209d58972c0ceba963f080a3
kvmn2000 <72999480+kvmn2000@users.noreply.github.com>
2021-02-16 14:08:43 -0500

    MapLoader implementation

--- MapLoader.cpp
+++ MapLoader.cpp
@@ -1,34 +1,64 @@
 #include "MapLoader.h"
 
-MapLoader::MapLoader(string name) {
+//default constructor
+MapLoader::MapLoader() {
 	map = new Map();
-	fpath = name;
-	readFile(name);
+	fpath = "";
 }
 
+//constructor with string parameter, calls readfile
+MapLoader::MapLoader(string path) {
+	map = new Map();
+	fpath = path;
+	valid = readFile(fpath);
+}
+
+MapLoader::~MapLoader() {
+	delete map;
+	map = NULL;
+}
+
+//returns file path
 string MapLoader::getPath(MapLoader maploader) {
 	return fpath;
 }
 
-Map* MapLoader::load() {
-	return map;
+//if the map is valid, it will return the map, if it is invalid, it will return an empty map
+Map MapLoader::getMap() {
+	if (valid) {
+		return *map;
+	}
+	else {
+		return Map();
+	}
 }
 
-void MapLoader::readFile(string path) {
+//loads in a different file into map loader
+void MapLoader::load(string path) {
+	fpath = path;
+	valid = readFile(fpath);
+}
+
+//reads a file line by line then creates a graph of the map outlined in the file.
+//it reads the nodes, then the edges.
+bool MapLoader::readFile(string path) {
+	if (!validFile(path)) {
+		cout << "This file is invalid." << endl;
+		return false;
+	}
 	ifstream file_reader(path);
 	string current_continent = "";
 	string current_country = "";
 	bool readingContinent = false;
 	bool readingEdge = false;
+
 	for (string line; getline(file_reader, line);) {
-		//cout << line << endl;
 		if (line == "}" || line == "]") {
 			readingContinent = false;
 			readingEdge = false;
 		}
 		if (readingContinent) {
 			current_country = line;
-			//cout << current_country << endl;
 			
 			Country* country = new Country(current_country, current_continent);
 			
@@ -39,12 +69,11 @@ void MapLoader::readFile(string path) {
 		if (readingEdge) {
 			string neighbor = line;
 			map->getCountry(current_country)->addNeighbor(new Country(neighbor));
-			cout << "Added " << neighbor << " to " << current_country << endl;
+			//cout << "Added " << neighbor << " to " << current_country << endl;
 		}
 		if (line.find('{') != string::npos) {
 			readingContinent = true;
 			current_continent = line.substr(0, line.find('{'));
-			//cout << current_continent << endl;
 			Continent* continent = new Continent(current_continent);
 			map->addContinent(continent);
 		}
@@ -53,11 +82,18 @@ void MapLoader::readFile(string path) {
 			current_country = line.substr(0, line.find('['));
 		}
 	}
+
 	if (map->toValidate()) {
 		cout << "This map is valid." << endl;
+		return true;
 	}
 	else {
-		cout << "This map is invalid. Closing program." << endl;
-		exit(-1);
+		cout << "This map is invalid." << endl;
+		return false;
 	}
+}
+
+//only accepts .txt files
+bool MapLoader::validFile(string path) {
+	return path.substr(path.length() - 4, path.length() - 1) == ".txt";
 }
