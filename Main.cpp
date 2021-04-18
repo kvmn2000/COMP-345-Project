d22443608b468ff7653e17a130f685ec0f356a97
kvmn2000 <72999480+kvmn2000@users.noreply.github.com>
2021-04-18 18:11:14 -0400

    check

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@
 #include "MapLoader.h"
 #include "Cards.h"
 #include "Player.h"
+#include "ActionObserver.h"
 
 using namespace std;
 
@@ -11,9 +12,11 @@ int main(int argc, const char* argv[]) {
 
 	const string mapDirectory = "./Maps/";
 	GameEngine* game = new GameEngine(mapDirectory);
+	ActionObserver* actionObserver = new ActionObserver(game);
 	game->startUp();
 
 	game->~GameEngine();
+	delete actionObserver;
 	return 0;
 
 }
